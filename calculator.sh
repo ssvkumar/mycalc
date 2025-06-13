f38b6db47bb9b6b4343090e5ef238c5910c1b124
Gajanan Taur <gajanan.taur96@gmail.com>
2025-06-13 11:15:14 +0530

    addition function added

--- calculator.sh
+++ calculator.sh
@@ -5,6 +5,7 @@ read -p "Enter second number: " num2
 
 echo "--- Menu ---"
 echo "0. Exit"
+echo "1. Addition"
 echo "------------"
 
 read -p "Enter your choice: " choice
@@ -15,6 +16,11 @@ case $choice in
     echo "Exiting..."
     exit 0
     ;;
+
+    1)
+        res=$((num1 + num2))
+        echo "Addition: $res"
+        ;;
 
     *)
     echo "Invalid choice. Please try again."
